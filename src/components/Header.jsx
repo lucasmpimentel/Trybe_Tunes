a3341bc6a8125e40fbc613c024fb3be4568e739a
lucasmpimentel <lucas.mirandapimentel@gmail.com>
2022-02-06 15:20:51 -0300

    requisito 3 e organizaçao do Header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -22,10 +22,9 @@ export default class Header extends Component {
     }
     return (
       <header data-testid="header-component">
-        <h1>Trybe Tunes</h1>
+        <h3>Bem-Vindo</h3>
         <h2 data-testid="header-user-name">{ userName }</h2>
         <nav>
-          <span>Menu: </span>
           <button type="button">
             <Link to="/search">Buscar</Link>
           </button>
